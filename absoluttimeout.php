122da1f81bd857def029236596df1c2e83cdfbb2
Irasil <simon.stauffer@student.ipso.ch>
2023-06-22 22:06:59 +0200

    Bug Submit Form nach ablauf der Session behoben

--- absoluttimeout.php
+++ absoluttimeout.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Setze das absolute Timeout (in Sekunden)
-$absoluteTimeout = 1800; // Timeout von 30 Minuten 1800
+$absoluteTimeout = 5; // Timeout von 30 Minuten 1800
 
 // Überprüfe, ob die Session bereits gestartet wurde
 if (isset($_SESSION['start_time'])) {
@@ -11,7 +11,9 @@ if (isset($_SESSION['start_time'])) {
 
     // Überprüfe das absolute Timeout
     if ($elapsedTime > $absoluteTimeout) {
-        
+        session_start();
+        session_unset();
+        session_destroy();
         header('Location: logout.php');
     }
     
